4b6147bb5a976a1da88b14872412e06557faae39
wlstmd <jinwoo8288@gmail.com>
2025-03-14 22:39:20 +0900

    initial commit

---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,37 @@
+import { Link } from 'src/link/entities/link.entity';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  Unique,
+} from 'typeorm';
+
+@Entity()
+@Unique(['email'])
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  loginType: 'email';
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  nickName: string;
+
+  @Column({ nullable: true })
+  imageUri?: string;
+
+  @Column({ nullable: true })
+  hashedRefreshToken?: string;
+
+  @OneToMany(() => Link, (link) => link.user)
+  links: Link[];
+}
